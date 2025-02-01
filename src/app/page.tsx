120dbb03da5582b78e74704d846712e4e48c3ec7
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-02-01 13:48:42 -0500

    started working on a temp simple landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Landing from "@/app/(marketing)/landing/page";
+import { Navbar } from "@/components/marketing/navbar";
+import { Footer } from "@/components/marketing/footer";
+
 export default function Home() {
-  return <div>hi</div>;
+  return (
+    <div className="nondashboard-layout">
+      <Navbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
+      <Footer />
+    </div>
+  );
 }
