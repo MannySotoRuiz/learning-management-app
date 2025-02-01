e497fb7f8c251f17db69b23e3b902d198896bd0f
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-02-01 15:26:38 -0500

    created course controller and routes, get course list and get course by ID

---
+++ src/controllers/courseController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+
+export const listCourses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  // https://example.com/path?key1=value1&key2=value2
+  const { category } = req.query; // query used for optional params
+  try {
+    const courses =
+      category && category !== "all"
+        ? await Course.scan("category").eq(category).exec()
+        : await Course.scan().exec();
+
+    res.json({ message: "Courses retrieved successfully", data: courses });
+  } catch (error) {
+    res.status(500).send({ message: "Error retrieving courses list" });
+  }
+};
+
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+  // https://example.com/path/{param}
+  const { courseId } = req.params; // param used for required params
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found" });
+      return;
+    }
+
+    res.json({ message: "Course retrieved successfully", data: course });
+  } catch (error) {
+    res.status(500).send({ message: "Error retrieving course" });
+  }
+};
+
+// export const name = async (
+//   req: Request,
+//   res: Response
+// ): Promise<void> => {
+//   try {
+
+//   } catch (error) {
+//    res.status(500).send({ message: "Error" })
+//   }
+// }
