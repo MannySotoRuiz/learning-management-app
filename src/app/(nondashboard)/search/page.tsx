b66ea5dfcea26ab2335dd7d0bf2c505c9327cd74
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-02-01 16:35:50 -0500

    created search page to browse courses

---
+++ src/app/(nondashboard)/search/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { motion } from "framer-motion";
+import { useGetCoursesQuery } from "@/state/api";
+
+import Loading from "@/components/loading";
+import { CourseCardSearch } from "@/components/course-card-search";
+import { SelectedCourse } from "./selected-course";
+
+const SearchPage = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const { data: courses, isError, isLoading } = useGetCoursesQuery({});
+  const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
+
+  useEffect(() => {
+    if (courses) {
+      if (id) {
+        const course = courses.find((c) => c.courseId === id);
+        setSelectedCourse(course || courses[0]);
+      } else {
+        setSelectedCourse(courses[0]);
+      }
+    }
+  }, [courses, id]);
+
+  if (isLoading) return <Loading />;
+  if (isError || !courses) return <div>Failed to fetch courses</div>;
+
+  const handleCourseSelect = (course: Course) => {
+    setSelectedCourse(course);
+    router.push(`/search?id=${course.courseId}`);
+  };
+
+  const handleEnrollNow = (courseId: string) => {
+    router.push(`/checkout?step=1&id=${courseId}&showSignUp=false`);
+  };
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="search"
+    >
+      <h1 className="search__title">List of available courses</h1>
+      <h2 className="search__subtitle">{courses.length} courses available</h2>
+      <div className="search__content">
+        <motion.div
+          initial={{ y: 40, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          viewport={{ amount: 0.4 }}
+          className="search__courses-grid"
+        >
+          {courses.map((course) => (
+            <CourseCardSearch
+              key={course.courseId}
+              isSelected={selectedCourse?.courseId === course.courseId}
+              course={course}
+              onClick={() => handleCourseSelect(course)}
+            />
+          ))}
+        </motion.div>
+
+        {selectedCourse && (
+          <motion.div
+            initial={{ y: 40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="search__selected-course"
+          >
+            <SelectedCourse
+              course={selectedCourse}
+              handleEnrollNow={handleEnrollNow}
+            />
+          </motion.div>
+        )}
+      </div>
+    </motion.div>
+  );
+};
+
+export default SearchPage;
