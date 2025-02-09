63f1db9224924b1505524ba49e84444e3a1781ac
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-02-08 19:04:37 -0500

    created controller to fetch user's enrolled courses

---
+++ src/controllers/userCourseProgressController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+import { v4 as uuidv4 } from "uuid";
+import { getAuth } from "@clerk/express";
+import UserCourseProgress from "../models/userCourseProgressModel";
+
+export const getUserEnrolledCourses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId } = req.params;
+  const auth = getAuth(req);
+
+  if (!auth || auth.userId !== userId) {
+    res.status(403).json({ message: "Access denied" });
+    return;
+  }
+
+  try {
+    const enrolledCourses = await UserCourseProgress.query("userId")
+      .eq(userId)
+      .exec();
+    const courseIds = enrolledCourses.map((item: any) => item.courseId);
+    const courses = await Course.batchGet(courseIds);
+    res.json({
+      message: "Enrolled courses retrieved successfully",
+      data: courses,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving enrolled courses", error });
+  }
+};
