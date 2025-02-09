b21d346946137626bc4b8f2a01f91f492eb6ffa8
MannySotoRuiz <sotoruiz.emmanuel@gmail.com>
2025-02-08 23:59:20 -0500

    created user course progress routes

--- src/controllers/userCourseProgressController.ts
+++ src/controllers/userCourseProgressController.ts
@@ -3,6 +3,7 @@ import Course from "../models/courseModel";
 import { v4 as uuidv4 } from "uuid";
 import { getAuth } from "@clerk/express";
 import UserCourseProgress from "../models/userCourseProgressModel";
+import { calculateOverallProgress, mergeSections } from "../utils/utils";
 
 export const getUserEnrolledCourses = async (
   req: Request,
@@ -30,5 +31,75 @@ export const getUserEnrolledCourses = async (
     res
       .status(500)
       .json({ message: "Error retrieving enrolled courses", error });
+  }
+};
+
+export const getUserCourseProgress = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, courseId } = req.params;
+
+  try {
+    const progress = await UserCourseProgress.get({ userId, courseId });
+    if (!progress) {
+      res
+        .status(404)
+        .json({ message: "Course progress not found for this user" });
+      return;
+    }
+    res.json({
+      message: "Course progress retrieved successfully",
+      data: progress,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error retrieving user course progress", error });
+  }
+};
+
+export const updateUserCourseProgress = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, courseId } = req.params;
+  const progressData = req.body;
+
+  try {
+    let progress = await UserCourseProgress.get({ userId, courseId });
+
+    if (!progress) {
+      // If no progress exists, create initial progress
+      progress = new UserCourseProgress({
+        userId,
+        courseId,
+        enrollmentDate: new Date().toISOString(),
+        overallProgress: 0,
+        sections: progressData.sections || [],
+        lastAccessedTimestamp: new Date().toISOString(),
+      });
+    } else {
+      // Merge existing progress with new progress data
+      progress.sections = mergeSections(
+        progress.sections,
+        progressData.sections || []
+      );
+      progress.lastAccessedTimestamp = new Date().toISOString();
+      progress.overallProgress = calculateOverallProgress(progress.sections);
+    }
+
+    await progress.save();
+
+    res.json({
+      message: "",
+      data: progress,
+    });
+  } catch (error) {
+    console.error("Error updating progress:", error);
+    res.status(500).json({
+      message: "Error updating user course progress",
+      error,
+    });
   }
 };
